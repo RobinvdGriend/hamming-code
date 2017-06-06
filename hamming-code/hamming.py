ce918c0cefc7781f90180dfd4f0bc067ed001d07
Thomas288 <t.j.koopman@students.uu.nl>
2017-06-06 09:23:44 +0200

    checker

--- hamming-code/hamming.py
+++ hamming-code/hamming.py
@@ -30,6 +30,18 @@ def encodemessage(message):
   vector_with_paritybits = encoding_matrix*vector
   return vector_with_paritybits.getbinary()
 
+
+def checkmessage(message):
+  #Turns the message into a column vector
+  vectorlist = []
+  for i in range(4):
+    vectorlist.append([int(message[i])])
+  vector = Matrix(vectorlist)
+  #Encodes with the encode matrix
+  vector_with_paritybits = checking_matrix*vector
+  return vector_with_paritybits.getbinary()
+
+
 #Example:
 #boodschap = input('Vul hier je boodschap in: ')
 #boodschapbinair = map(bin, bytearray(boodschap, 'utf8'))
