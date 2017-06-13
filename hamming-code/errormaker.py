89352e989d607dc7371dad110812ffa857a7975a
TimWaroux <t.waroux@gmail.com>
2017-06-13 12:12:22 +0200

    Added errormaker function in apart bestand

---
+++ hamming-code/errormaker.py
@@ -0,0 +1,13 @@
+import random
+from copy import deepcopy
+from matrix import Matrix
+
+def errormaker(encodelist):
+    errorlist = deepcopy(encodelist)
+    for i in range(len(Matrix(errorlist).values)):
+        r = random.randint(0,6)
+        if errorlist[i].values[r][0] == 1:
+            errorlist[i].values[r][0] = 0
+        else: errorlist[i].values[r][0] = 1
+    return errorlist
+
