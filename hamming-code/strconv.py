43320b534c71320c8816715dc4c96e45854c419f
RobinvdGriend <robinvandegriend@gmail.com>
2017-06-13 18:57:38 +0200

    codelist_to_str is klaar en werkt

--- hamming-code/strconv.py
+++ hamming-code/strconv.py
@@ -14,11 +14,18 @@ def str_to_codelist(string):
     return codelist
 
 def codelist_to_str(codelist):
+    #A codelist is a list of codes, a code being a 4 bit byte
     chars = []
-    for code in codelist:
-        bits = code.values[0]
-        #Still a problem with the chr() function call, "hoi" gets
-        #translated into weird characters
+
+    #A character is 2 bytes, thus we need every 2 bytes
+    #in the codelist, a left one and a right one. Also notice
+    #the horrid interface of Matrix() where you need to use .values[0]
+    #on every matrix to get something usable
+
+    code_pairs = [(codelist[i].values[0], codelist[i + 1].values[0]) for i in range(0, len(codelist) - 1, 2)]
+
+    for lcode,rcode in code_pairs:
+        bits = lcode + rcode
         chars.append(chr(int(''.join([str(bit) for bit in bits]), 2)))
     return "".join(chars)
 
