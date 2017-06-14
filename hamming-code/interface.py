f9a77ea4ee3d38dcfad76f6b9625f19315e3da1a
BuildTools <unconfigured@null.spigotmc.org>
2017-06-14 15:15:33 +0200

    Message_to_binstring and binstring_to_message added

--- hamming-code/interface.py
+++ hamming-code/interface.py
@@ -1,2 +1,22 @@
+from matrix import Matrix
 print('Vul hier uw boodschap in: ')
+message = input()
+def message_to_binstring(message):
+    bytelist = bytearray(message, 'ascii')
+    binstring = ''
+    for byte in bytelist:
+        for digit in list(bin(byte)[2:].zfill(8)):
+            binstring = binstring + digit
+    return binstring
 
+def binstring_to_message(binstring):
+    message = ''
+    while binstring != '':
+        #Every letter is represented by 8 1's and 0's, and the value of the binary number is the letter his chr()
+        letter = chr(int(binstring[:8], 2))
+        message = message + letter
+        binstring = binstring[8:]
+    return message
+    
+print(message_to_binstring(message))
+print(binstring_to_message(message_to_binstring(message)))
