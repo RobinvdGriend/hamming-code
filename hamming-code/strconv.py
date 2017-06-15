4c199eab9a0e72c65d8d93285200826cbeda99ff
RobinvdGriend <robinvandegriend@gmail.com>
2017-06-15 14:01:44 +0200

    Functie toegevoegd die binarystring omzet in codelist

--- hamming-code/strconv.py
+++ hamming-code/strconv.py
@@ -2,16 +2,16 @@ from matrix import Matrix
 
 
 def str_to_codelist(string):
-    # Bytes are collections of 4 bits in this case
+    # bytes are collections of 4 bits in this case
     codelist = []
 
     bytelist = bytearray(string, 'ascii')
 
-    # For every letter removes b from the string
+    # for every letter removes b from the string
     for byte in bytelist:
         code = [int(x) for x in list(bin(byte)[2:].zfill(8))]
-        codelist.append(Matrix([code[:4]]))
-        codelist.append(Matrix([code[4:]]))
+        codelist.append(matrix([code[:4]]))
+        codelist.append(matrix([code[4:]]))
     return codelist
 
 
@@ -20,9 +20,9 @@ def codelist_to_str(codelist):
     chars = []
 
     # A character is 2 bytes, thus we need every 2 bytes
-    # in the codelist, a left one and a right one. Also notice
+    # in the codelist, a left one and a right one. <rant> Also notice
     # the horrid interface of Matrix() where you need to use .values[0]
-    # on every matrix to get something usable
+    # on every matrix to get something usable </rant>
 
     code_pairs = [(codelist[i].values[0], codelist[i + 1].values[0])
                   for i in range(0, len(codelist) - 1, 2)]
@@ -30,4 +30,22 @@ def codelist_to_str(codelist):
     for lcode, rcode in code_pairs:
         bits = lcode + rcode
         chars.append(chr(int(''.join([str(bit) for bit in bits]), 2)))
+    return "".join(chars)
+
+def binary_to_codelist(binarystring,codelength=7):
+    codelist = []
+    
+    bytelist = [binarystring[i:i+codelength] for i in range(0,len(binarystring),codelength)]
+
+    for byte in bytelist:
+        code = list(byte)
+        codelist.append(Matrix([code]))
+    return codelist
+
+def codelist_to_binary(codelist):
+    chars = []
+    
+    for code in codelist:
+        bits = code.values[0]
+        chars.append(''.join([str(bit) for bit in bits]))
     return "".join(chars)
